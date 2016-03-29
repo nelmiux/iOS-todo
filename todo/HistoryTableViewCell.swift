c7af66eabc8c21373bc158bcdf9f55eb4faf0f6a
Quyen <q.castellanos@yahoo.com>
2016-03-29 16:06:06 -0500

    Beginning work on history UI

---
+++ todo/HistoryTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  HistoryTableViewCell.swift
+//  todo
+//
+//  Created by Quyen Castellanos on 3/29/16.
+//  Copyright © 2016 cs378. All rights reserved.
+//
+
+import UIKit
+
+class HistoryTableViewCell: UITableViewCell {
+
+    // UI Components
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var userPhoto: UIImageView!
+    @IBOutlet weak var dotsLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
