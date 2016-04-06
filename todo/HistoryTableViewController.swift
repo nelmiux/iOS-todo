89747f4853f1c5fafe8bf87a886f0013b484973d
Quyen <q.castellanos@yahoo.com>
2016-04-06 13:49:48 -0500

    Beginning to prepare for segue to show correct user profile

--- todo/HistoryTableViewController.swift
+++ todo/HistoryTableViewController.swift
@@ -165,14 +165,17 @@ class HistoryTableViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        /* if segue.identifier == "displayUserProfile" {
+            let destVC = segue.destinationViewController as! ProfileViewController
+            let idx = sender?.indexPath.row
+            print("Selected cell \(idx)")
+            // destVC.isOwnProfile = false
+        } */
     }
-    */
 
 }
